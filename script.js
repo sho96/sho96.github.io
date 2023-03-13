efca09b608edbb53e198ba5020b506bb2df1201e
sho96 <81459356+sho96@users.noreply.github.com>
2023-03-13 18:40:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const minInput = document.querySelector(".min");
 const maxInput = document.querySelector(".max");
 
 const generateRandomNumber = () => {
-    const minNumber = minInput["value"];
-    const maxNumber = maxInput["value"];
+    const minNumber = parseInt(minInput["value"]);
+    const maxNumber = parseInt(maxInput["value"]);
 
-    if (minNumber == "" || maxNumber == ""){
+    if (minNumber == NaN || maxNumber == NaN){
         resultParagraph.innerHTML = "the min and the max values aren't set."
         return;
     }
 
-    const number = Math.round((Math.random()*(maxNumber-minNumber)))
+    const number = Math.round((Math.random()*(maxNumber-minNumber))+minNumber);
     console.log(number);
     console.log(number+minNumber);
     resultParagraph.innerHTML = "The number is " + number;
